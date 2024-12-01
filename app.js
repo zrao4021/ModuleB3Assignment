a065f8f27c37c513b087d080cc364c6ed833d013
zrao4021 <zrao4021@gmail.com>
2024-12-01 17:24:49 +0500

    third assignment of module b

---
+++ app.js
@@ -0,0 +1,228 @@
+// exp 1.
+
+// ans is NaN.
+
+// exp2.
+
+// ans is 0
+
+// exp 3
+
+// ans is NanResult
+
+// exp 4
+
+// ans is 0
+
+// exp 5
+
+// ans is true
+
+// exp 6
+
+//  ans is 11
+
+// exp 7
+
+// ans is 9
+
+// exp 8
+
+// "value"
+
+// exp 9
+
+// ans is "3result"
+
+// exp 10
+
+// ans is 1
+
+// exp 11
+
+// ans is NaN
+
+// exp 12
+
+// ans is "result"
+
+// exp 13
+
+// ans is 4
+
+// exp 14
+
+// ans is "end"
+
+// exp 15
+
+// ans is "NaNresult8"
+
+// exp 16
+
+// ans is null
+
+// exp 17
+
+// ans is 4
+
+// exp 18
+
+// ans is 0
+
+// exp 19
+
+// ans is "foo"
+
+// exp 20
+
+// ans is "end5"
+
+// exp 21
+
+// ans is "testfalse"
+
+// exp 22
+
+// ans is "test"
+
+// exp 23
+
+// ans is "result"
+
+// exp 24
+
+// ans is 2
+
+// exp 25
+
+// ans is 0
+
+// exp 26
+
+// ans is 7
+
+// exp 27
+
+// ans is "start0"
+
+// exp 28
+
+// ans is 3
+
+// exp 29
+
+// ans is "NaNresult"
+
+// exp 30
+
+// ans is "0end"
+
+// exp 31
+
+// ans is "3"
+
+// exp 32
+
+// ans is 0
+
+// exp 33
+
+// ans is "end"
+
+// exp 34
+
+// ans is "hello0"
+
+// exp 35
+
+// ans is false
+
+// exp 36
+
+// ans is 2
+
+// exp 37
+
+// ans is "2test"
+
+// exp 38
+
+// ans is "2end"
+
+// exp 39
+
+// ans is 2
+
+// exp 40
+
+// ans is 6
+
+// exp 41
+
+// ans is "hello"
+
+// exp 42
+
+// ans is NaN
+
+// exp 43
+
+// ans is "start5"
+
+// exp 44
+
+// ans is "value1"
+
+// exp 45
+
+// ans is 8
+
+// exp 46
+
+// ans is "test"
+
+// exp 47
+
+// ans is 8
+
+// exp 48
+
+// ans is naN
+
+// exp 49
+
+// ans is 16
+
+// exp 50
+
+// ans is "true"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
